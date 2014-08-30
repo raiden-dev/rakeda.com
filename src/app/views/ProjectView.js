e7789fa4839c55c0fa6a45d2ef377615e292d351
Ian Rakeda <proflex@ya.ru>
2014-08-30 18:38:08 +0400

    Add loader

--- src/app/views/ProjectView.js
+++ src/app/views/ProjectView.js
@@ -24,6 +24,57 @@ define([
     },
 
     onAfterRender: function () {
+      var $splash = this.$el.find('.project__splash'),
+          $objects = this.$el.find('.project__objects'),
+          $loader = this.$el.find('.loader');
+
+      $splash.hide();
+      $objects.hide();
+      $loader.show();
+
+      var $img = this.$el.find('img'),
+          imgCount = $img.length,
+          imgLoaded = 0,
+          imgLoadDeferred = new $.Deferred();
+
+      var $video = this.$el.find('video'),
+          videoCount = $video.length,
+          videoLoaded = 0,
+          videoLoadDeferred = new $.Deferred();
+
+      if ($video.length) {
+        $video.on('loadeddata', function () {
+          videoLoaded++;
+
+          if (videoLoaded === videoCount) {
+            videoLoadDeferred.resolve();
+          }
+        });
+      }
+      else {
+        videoLoadDeferred.resolve();
+      }
+
+      if ($img.length) {
+        $img.on('load', function () {
+          imgLoaded++;
+
+          if (imgLoaded === imgCount) {
+            imgLoadDeferred.resolve();
+          }
+        });
+      }
+      else {
+        imgLoadDeferred.resolve();
+      }
+
+      $.when(imgLoadDeferred, videoLoadDeferred)
+        .done(function () {
+          $loader.hide();
+          $splash.show();
+          $objects.show();
+        });
+
       $(window).off('scroll').on('scroll', function () {
         var $arrow = this.$el.find('.js-scroll'),
             windowHeight = $(window).height(),
